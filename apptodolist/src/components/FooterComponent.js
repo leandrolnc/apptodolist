fc58c1f37b1f7b59c6a1a3e3269264c86be7fff3
Leandro <leandrolnc@hotmail.com>
2018-10-18 14:42:54 -0300

    estrutura inicial e dependencias

---
+++ apptodolist/src/components/FooterComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+
+function Footer(props) {
+    return(
+    <div className="footer">
+        <div className="container">
+            <div className="row justify-content-center">             
+                <div className="col-auto">
+                    <p>© Copyright 2018 </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default Footer;
---
+++ apptodolist/src/components/HomeComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Home(props) {
+
+    return(
+        <div className="container">
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <h1>Div 1</h1>
+                </div>
+                <div className="col-12 col-md m-1">
+                    <h2>Div 2</h2>
+                </div>
+                <div className="col-12 col-md m-1">
+                    <h3>Div 3</h3>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
